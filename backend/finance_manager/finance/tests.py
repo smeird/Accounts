b264a52602bfb707154dd9f889b3244ef231f6d4
smeird <dom@smeird.com>
2025-08-01 13:03:31 +0100

    Delete backend directory

--- backend/finance_manager/finance/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .models import Account
-
-class AccountModelTest(TestCase):
-    def test_create_account(self):
-        account = Account.objects.create(name='Checking')
-        self.assertEqual(account.name, 'Checking')
