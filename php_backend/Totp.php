4bd7bddc240275e6cb04576aaf2d65b48cb58924
smeird <dom@smeird.com>
2025-08-21 12:17:55 +0100

    Encode chld parameter in TOTP QR links

---
+++ php_backend/Totp.php
@@ -0,0 +1,80 @@
+<?php
+class Totp {
+    private static $alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+    public static function generateSecret($length = 16) {
+        return self::base32Encode(random_bytes($length));
+    }
+
+    public static function getQrCodeUrl($username, $secret) {
+        $issuer = 'Accounts';
+        $otpauth = sprintf('otpauth://totp/%s?secret=%s&issuer=%s',
+            rawurlencode($username),
+            $secret,
+            rawurlencode($issuer)
+        );
+        // Encode pipe character in chld parameter to avoid 404 responses
+        return 'https://chart.googleapis.com/chart?chs=200x200&chld=M%7C0&cht=qr&chl=' . urlencode($otpauth);
+    }
+
+    public static function verifyCode($secret, $code, $window = 1) {
+        $key = self::base32Decode($secret);
+        $time = floor(time() / 30);
+        for ($i = -$window; $i <= $window; $i++) {
+            if (hash_equals(self::calcTotp($key, $time + $i), $code)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function getCode($secret) {
+        $key = self::base32Decode($secret);
+        return self::calcTotp($key, floor(time() / 30));
+    }
+
+    private static function calcTotp($key, $timeSlice) {
+        $binaryTime = pack('N*', 0, $timeSlice);
+        $hash = hash_hmac('sha1', $binaryTime, $key, true);
+        $offset = ord(substr($hash, -1)) & 0x0F;
+        $value = unpack('N', substr($hash, $offset, 4))[1] & 0x7fffffff;
+        return str_pad($value % 1000000, 6, '0', STR_PAD_LEFT);
+    }
+
+    private static function base32Encode($data) {
+        $alphabet = self::$alphabet;
+        $binaryString = '';
+        foreach (str_split($data) as $char) {
+            $binaryString .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT);
+        }
+        $chunks = str_split($binaryString, 5);
+        $result = '';
+        foreach ($chunks as $chunk) {
+            $chunk = str_pad($chunk, 5, '0', STR_PAD_RIGHT);
+            $result .= $alphabet[bindec($chunk)];
+        }
+        return $result;
+    }
+
+    private static function base32Decode($b32) {
+        $alphabet = self::$alphabet;
+        $b32 = strtoupper($b32);
+        $b32 = preg_replace('/[^A-Z2-7]/', '', $b32);
+        $binaryString = '';
+        foreach (str_split($b32) as $char) {
+            $index = strpos($alphabet, $char);
+            if ($index !== false) {
+                $binaryString .= str_pad(decbin($index), 5, '0', STR_PAD_LEFT);
+            }
+        }
+        $bytes = str_split($binaryString, 8);
+        $result = '';
+        foreach ($bytes as $byte) {
+            if (strlen($byte) === 8) {
+                $result .= chr(bindec($byte));
+            }
+        }
+        return $result;
+    }
+}
+?>
---
+++ php_backend/public/totp_disable.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../Totp.php';
+ini_set('session.cookie_secure', '1');
+session_start();
+header('Content-Type: application/json');
+$input = json_decode(file_get_contents('php://input'), true);
+$username = $input['username'] ?? ($_SESSION['username'] ?? '');
+if ($username === '') {
+    echo json_encode(['disabled' => false, 'error' => 'Username required']);
+    exit;
+}
+$file = __DIR__ . '/../totp_secrets.json';
+$users = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+if (isset($users[$username])) {
+    unset($users[$username]);
+    file_put_contents($file, json_encode($users, JSON_PRETTY_PRINT));
+    echo json_encode(['disabled' => true]);
+} else {
+    echo json_encode(['disabled' => false, 'error' => 'No 2FA to disable']);
+}
+?>
