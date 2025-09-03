b0f3120373289e2fe60f151608486778aba12085
smeird <dom@smeird.com>
2025-09-03 16:10:57 +0100

    Switch Responses API parameter to text.format

--- php_backend/NaturalLanguageReportParser.php
+++ php_backend/NaturalLanguageReportParser.php
@@ -71,7 +71,7 @@ class NaturalLanguageReportParser {
                 ['role' => 'user', 'content' => $prompt],
             ],
             'temperature' => 0,
-            'response_format' => ['type' => 'json_object'],
+            'text' => ['format' => 'json_object'],
         ];
 
         $ch = curl_init('https://api.openai.com/v1/responses');
