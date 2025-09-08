cf6e74db2671da63c89595b45126113e55306618
smeird <dom@smeird.com>
2025-09-08 17:12:51 +0100

    bundle Tailwind CSS locally

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./frontend/**/*.{html,js,php}",
+    "./*.{php,html}",
+    "./php_backend/**/*.php"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
